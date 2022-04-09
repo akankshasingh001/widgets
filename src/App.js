afaecd82ab258d9437ea4d285f27d2aa03296cf2
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-09 12:47:53 -0400

    hooks used and show accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Accordion from './Components/Accordion';
+// import AccordianItems from './Components/AccordionItems';
 
 const App = () => {
-  return <div>Widgets App</div>;
+  const items = [
+    {
+      title: 'What is React?',
+      content:
+        'React is a JavaScript library that is used for building user interface like web application ,SPA etc.'
+    },
+    {
+      title: 'What is JSX?',
+      content:
+        'JSX is a XML-like syntax extension to ECMAScript (the acronym stands for JavaScript XML). Basically it just provides syntactic sugar for the React.createElement() function, giving us expressiveness of JavaScript along with HTML like template syntax.'
+    },
+    {
+      title: 'What is the difference between Element and Component?',
+      content:
+        'An Element is a plain object describing what you want to appear on the screen in terms of the DOM nodes or other components. Elements can contain other Elements in their props. Creating a React element is cheap. Once an element is created, it is never mutated.'
+    }
+  ];
+
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
