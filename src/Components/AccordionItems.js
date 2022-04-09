fba7c43b840b6c432643ffd342c265b405c2afd0
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-09 12:49:02 -0400

    finished

---
+++ src/Components/AccordionItems.js
@@ -0,0 +1,19 @@
+const items = [
+  {
+    title: 'What is React?',
+    description:
+      'React is a JavaScript library that is used for building user interface like web application ,SPA etc.'
+  },
+  {
+    title: 'What is JSX?',
+    description:
+      'JSX is a XML-like syntax extension to ECMAScript (the acronym stands for JavaScript XML). Basically it just provides syntactic sugar for the React.createElement() function, giving us expressiveness of JavaScript along with HTML like template syntax.'
+  },
+  {
+    title: 'What is the difference between Element and Component?',
+    description:
+      'An Element is a plain object describing what you want to appear on the screen in terms of the DOM nodes or other components. Elements can contain other Elements in their props. Creating a React element is cheap. Once an element is created, it is never mutated.'
+  }
+];
+
+export default items;
